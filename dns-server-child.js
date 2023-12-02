d02c0a074ef4a3962f7be9f20597d10c299af0ef
Aioros <aioros83@gmail.com>
2023-12-02 13:02:35 -0500

    Preparing support for executable DNS server with pkg

--- dns-server-child.js
+++ dns-server-child.js
@@ -3,8 +3,13 @@
 const DnsProxyServer = require("@aiorosdev/dns-proxy-lib");
 const io = require('socket.io')();
 
-console.log("config received: " + process.argv[3]);
-let config = JSON.parse(Buffer.from(process.argv[3], "base64").toString("ascii"));
+let config;
+
+try {
+    config = JSON.parse(Buffer.from(process.argv[3], "base64").toString("ascii"));
+} catch(ex) {
+    config = {};
+}
 console.log(config);
 
 let dnsProxyServer = new DnsProxyServer(config);
--- dns-server.js
+++ dns-server.js
@@ -1,6 +1,7 @@
 const DnsProxyServer = require("@aiorosdev/dns-proxy-lib");
-const sudo = require('sudo-prompt');
+const sudo = require("sudo-prompt");
 const { io } = require("socket.io-client");
+const fs = require("fs");
 
 function getSocket() {
     return io("http://127.0.0.1:3000");
@@ -14,9 +15,21 @@ class DnsServer {
     }
 
     runSeparateProcess() {
+        console.log("exe exists: " + fs.existsSync("./dns-server-child.exe"));
     	console.log("Couldn't run DNS server. Root/administrator permissions might be needed, trying to request them for a child process DNS server.");
+        // Find executable if available, otherwise run the node script
+        let executable = "node ./dns-server-child.js";
+        if (process.platform == "win32" && fs.existsSync("./dns-server-child.exe")) {
+            executable = "./dns-server-child.exe";
+        } else if (fs.existsSync("./dns-server-child")) {
+            executable = "./dns-server-child";
+        }
+        if (process.platform == "win32") {
+            executable = executable.replace(/\//g, "\\");
+        }
+        let command = executable + " --config " + Buffer.from(JSON.stringify(this.config)).toString("base64");
 	    this.inProcess = false;
-	    sudo.exec("node ./dns-server-child.js --config " + Buffer.from(JSON.stringify(this.config)).toString("base64"), {name: "Console Streaming Server"}, (error, stdout, stderr) => {
+	    sudo.exec(command, {name: "Console Streaming Server"}, (error, stdout, stderr) => {
             if (error) {
                 console.log('stdout: ' + stdout);
                 console.log('stderr: ' + stderr);
